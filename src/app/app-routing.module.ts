224d020eb4f619bc9aaf8fdaf6cd60c1e383d178
Shayan <shayansaif@hotmail.com>
2021-05-07 16:30:36 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WatchlistFormComponent } from './watchlist/watchlist-form/watchlist-form.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WatchlistComponent},
+  {path: 'add', component: WatchlistFormComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
