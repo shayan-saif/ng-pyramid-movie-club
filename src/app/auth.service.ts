0d0c852b815d714308172dc6fa156dc7644da82d
Shayan <shayansaif@hotmail.com>
2021-05-14 21:20:45 -0400

    fixed styling for watchlist component

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
       secret: registerInfo.secret
     }
     
-    return this.http.post<IUser>('http://pyramidmovieclub-env.eba-2f3jnpr3.us-east-1.elasticbeanstalk.com/api/auth/register', user);
+    return this.http.post<IUser>('http://localhost:3000/api/auth/register', user);
   }
 
   loginUser(loginInfo: ILogin) {
@@ -30,13 +30,13 @@ export class AuthService {
       password: loginInfo.password,
     }
 
-    return this.http.post<IUser>('http://pyramidmovieclub-env.eba-2f3jnpr3.us-east-1.elasticbeanstalk.com/api/auth/login', user);
+    return this.http.post<IUser>('http://localhost:3000/api/auth/login', user);
   }
 
   
 
   logout() {
-    this.http.get('http://pyramidmovieclub-env.eba-2f3jnpr3.us-east-1.elasticbeanstalk.com/api/auth/logout').subscribe(() => {
+    this.http.get('http://localhost:3000/api/auth/logout').subscribe(() => {
       this.user.next(null);
     });
   }
