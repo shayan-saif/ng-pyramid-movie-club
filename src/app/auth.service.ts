93ae22c2922e99206801854c3b71710086a069b8
Shayan <shayansaif@hotmail.com>
2021-05-17 14:13:29 -0400

    fixed logout route

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -39,7 +39,7 @@ export class AuthService {
   
 
   logout() {
-    this.http.get('BACKEND_URL/logout').subscribe(() => {
+    this.http.get(BACKEND_URL + '/logout').subscribe(() => {
       this.user.next(null);
     });
   }
